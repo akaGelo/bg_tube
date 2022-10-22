334f623ab383a87ff3c8c4d0a0e471a379b5d2a2
Oleg Vyukov <o.vyukov@alfacapital.ru>
2022-10-23 01:21:16 +0300

    draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:move_to_background/move_to_background.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return WillPopScope(
+      onWillPop: () async {
+        MoveToBackground.moveTaskToBack();
+        return false;
+      },
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AudioPlayerWidget(title: 'Flutter Demo Home Page'),
       ),
-      home: AudioPlayerWidget(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -39,8 +50,11 @@ class AudioPlayerWidget extends StatefulWidget {
 class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
   int _counter = 0;
   late String title = "MyApp";
+  late String _sharedText = "";
+  late StreamSubscription _intentDataStreamSubscription;
 
   late AudioPlayer _audioPlayer;
+  var _yt = YoutubeExplode();
   PlayerState _playerState = PlayerState.stopped;
 
   // bool get _isPlaying => _playerState == PlayerState.playing;
@@ -54,12 +68,61 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
         _playerState = PlayerState.stopped;
       });
     });
-    super.initState();
+    //end audio
+
+    initPlatformState();
+
+    // For sharing or opening urls/text coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String value) {
+      setState(() {
+        //https://youtu.be/sOOQSBCWZCI
+        _sharedText = value;
+      });
+    }, onError: (err) {
+      print("getLinkStream error: $err");
+    });
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialText()
+        .then((value) => {
+              setState(() {
+                print(value);
+                _sharedText = "test";
+              })
+            })
+        .onError((error, stackTrace) {
+      return {null};
+    });
+    // super.initState();
+  }
+
+  Future<void> _onBg() async {
+    MoveToBackground.moveTaskToBack();
+  }
+
+  Future<void> initPlatformState() async {
+    // You can provide either a video ID or URL as String or an instance of `VideoId`.
+    var video = await _yt.videos.get(
+        'https://youtube.com/watch?v=Dpp1sIL1m5Q'); // Returns a Video instance.
+
+    var title = video.title; // "Scamazon Prime"
+    var author = video.author; // "Jim Browning"
+    var duration = video.duration; // Instance of Duration - 0:19:48.00000
+
+    var manifest = await _yt.videos.streamsClient.getManifest('Dpp1sIL1m5Q');
+
+    var audios = manifest.audioOnly.sortByBitrate();
+
+    setState(() {
+      print(audios);
+    });
   }
 
   @override
   void dispose() {
     _audioPlayer.dispose();
+    _yt.close();
     super.dispose();
   }
 
@@ -67,6 +130,10 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
     await _audioPlayer.play(UrlSource(
         "https://file-examples.com/storage/fe4b4c6261634c76e91986b/2017/11/file_example_MP3_700KB.mp3"));
     print("test");
+
+    _onBg();
+
+    // SystemNavigator.pop();
   }
 
   @override
